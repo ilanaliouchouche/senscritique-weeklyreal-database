60dcacf05397481dccd10b866244293d3bb99725
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2023-12-17 14:12:10 +0100

    updated .gitignore and etl/extract.py :     - extraction of movie data/image & critic reviews

--- etl/extract.py
+++ etl/extract.py
@@ -1,20 +1,26 @@
 import requests
-import requests
-import json
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-class BaseExtract:
+class BaseFilmExtractor:
     
-    URL = "https://www.senscritique.com/liste/films_2023/3376363"
+    URL = "https://www.senscritique.com/films/sorties-cinema/"
 
-    def __init__(self, url=URL):
-        self.url = url
+    def __init__(self, year, url=URL):
+        self.url = f"{url}/{year}"
         self.urls_films = None
         self.informations = None
+    
+    @staticmethod
+    def extract_film_links_from_page(driver):
+        elements = driver.find_elements(By.CLASS_NAME, "Poster__SubLink-sc-yale2-2.jhmgpI")
+        return [element.get_attribute('href') for element in elements]
 
     @staticmethod
-    def get_number_of_pages(url):
+    def calculate_total_pages(url):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -26,29 +32,43 @@ class BaseExtract:
         else:
             return 1 
 
-    def get_urls_films(self):
-        urls_films = []
+    def extract_all_film_links(self, cpt = 4):
+        if cpt == 0:
+            raise Exception("Too many attempts")
+        n = self.calculate_total_pages(self.url)
+        driver = webdriver.Chrome()
+        driver.get(self.url)
 
-        pages = self.get_number_of_pages(self.url)
-              
-        for page in tqdm(range(1, pages + 1)):
-            url_page = f"{self.url}?page={page}"
-            response = requests.get(url_page)
-            soup = BeautifulSoup(response.content, 'html.parser')
+        time.sleep(5)
+        cookie_button = self.driver.find_element(By.ID, "didomi-notice-agree-button")
+        cookie_button.click()
+        time.sleep(5)
 
-            script = soup.find('script', {'type': 'application/ld+json'})
-            if script:
-                data = json.loads(script.string)
+        all_film_links = []
 
-                for element in data.get('itemListElement', []):
-                    film_url = element.get('url')
-                    if film_url:
-                        urls_films.append(film_url)
+        page_number = 1
+        while True:
+            film_links = self.extract_film_links_from_page(driver)
+            all_film_links.extend(film_links)
 
-        self.urls_films = urls_films
+            try:
+                page_number += 1
+                next_page_button = self.driver.find_element(By.XPATH, f"//nav[contains(@class, 'Pagination__WrapperPagination-sc-1h0mvsr-0')]//span[normalize-space()='{page_number}']")
+                next_page_button.click()
+                time.sleep(5)
+            except Exception as e:
+                if page_number != n:
+                    c = cpt - 1
+                    driver.quit()
+                    self.extract_all_film_links(cpt=c)
+                else:
+                    self.urls_films = all_film_links
+                    print(f"Done with page {page_number - 1}")
+                    driver.quit()
+                    break
     
     @staticmethod
-    def get_film_details(detail_url):
+    def extract_film_details(detail_url):
         response = requests.get(detail_url)
         soup = BeautifulSoup(response.content, 'html.parser')
         info_div = soup.find("div", class_="Text__SCText-sc-1aoldkr-0 Movie__Text-sc-1tik1a1-1 gATBvI kkLsHK")
@@ -56,13 +76,13 @@ class BaseExtract:
         if info_div:
             for span in info_div.find_all("span"):
                 text = span.text.strip()
-                if not text.endswith(':') and len(text) > 0:
+                if not text.endswith(':') and len(text) > 0 and " : " in text:
                     infos.append(text)
         infos_dict = {info.split(' : ', 1)[0].strip(): info.split(' : ', 1)[1].strip() for info in infos}
         return infos_dict
 
     @staticmethod
-    def get_title(detail_url):
+    def extract_film_title(detail_url):
         response = requests.get(detail_url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -75,7 +95,7 @@ class BaseExtract:
         raise Exception("Title not found")
     
     @staticmethod
-    def get_rate(url):
+    def extract_film_rating(url):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -84,28 +104,32 @@ class BaseExtract:
             return float(div_note.text.strip())  
         return None
     
-    def extract_all_film_details(self):
+    @staticmethod
+    def extract_image(url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        span_image = soup.find("span", class_="Poster__WrapperImage-sc-yale2-10 jPPcbD")
+
+        data_srcname = None
+        if span_image and 'data-srcname' in span_image.attrs:
+            data_srcname = span_image.attrs['data-srcname']
+        return data_srcname
+    
+    def extract_all_film_data(self):
         all_details = {}
         for url in tqdm(self.urls_films):
             detail_url = url + "/details"
-            details = self.get_film_details(detail_url)
-            title = self.get_title(detail_url)
+            details = self.extract_film_details(detail_url)
+            title = self.extract_film_title(detail_url)
             all_details[title] = details
             all_details[title]['url'] = url
-            all_details[title]['rate'] = self.get_rate(url)
+            all_details[title]['rate'] = self.extract_film_rating(url)
+            all_details[title]['image'] = self.extract_image(url)
         self.informations = all_details
-        
-    
-    
-
-    
 
 
-class Extract(BaseExtract):
+class Extract(BaseFilmExtractor):
 
     def __init__(self, url):
         super().__init__(url)
-    
-
-
-
