ec26eb45497b3d8803790728943a44663d90edf3
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2023-12-19 20:02:34 +0100

    New : class FilmTransformer compute dataframes before launching

--- etl/extract.py
+++ etl/extract.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
+import re
 
 class BaseFilmExtractor:
     
@@ -224,3 +226,62 @@ class CurrentMovieExtractor(BaseFilmExtractor):
     def __init__(self, url=URL):
         super().__init__(url)
         self.url = url
+
+
+class FilmTransformer:
+
+    def __init__(self, extractor : BaseFilmExtractor):
+        self.extractor = extractor
+        self.df_films, self.df_genres, self.df_producteurs, self.df_realisateurs, self.df_scenaristes, self.df_pays = self.__transform()
+    
+    def __transform(self):
+        informations = self.extractor.informations
+        films_cols  = ['url', 'rate', 'Date de sortie (France)', 'image', 'Bande originale', 'Groupe', 'Année', 'Durée']
+        df_films = pd.DataFrame(columns=['film',*films_cols])
+        df_genres = pd.DataFrame(columns=['film', 'genre'])
+        df_producteurs = pd.DataFrame(columns=['film', 'producteur'])
+        df_realisateurs = pd.DataFrame(columns=['film', 'realisateur'])
+        df_scenaristes = pd.DataFrame(columns=['film', 'scenariste'])
+        df_pays = pd.DataFrame(columns=['film', 'pays'])
+
+        for title, info in informations.items():
+            for key in list(info.keys()):
+                if re.match(r"^Prod.*", key):
+                    info['Producteurs'] = info.pop(key)
+                elif re.match(r"^Scénar.*", key):
+                    info['Scénaristes'] = info.pop(key)
+                elif re.match(r"^Genr.*", key):
+                    info['Genre'] = info.pop(key)
+                elif re.match(r"^Réal.*", key):
+                    info['Réalisateurs'] = info.pop(key)
+                elif re.match(r"^Pays d.*", key):
+                    info['Pays d\'origine'] = info.pop(key)
+
+            film_dict = dict(**{'film' : title}, **{key: info.get(key, None) for key in films_cols})
+        
+
+            df_films = df_films._append(film_dict, ignore_index=True)
+
+
+            for review in info.get('reviews', {}).get('Positives', []):
+                df_reviews = df_reviews._append({'film': title, 'is_negative': False, **review}, ignore_index=True)
+
+            for review in info.get('reviews', {}).get('Negatives', []):
+                df_reviews = df_reviews._append({'film': title, 'is_negative': True, **review}, ignore_index=True)
+
+            
+            for producteur in info.get('Producteurs', '').split(', '):
+                df_producteurs = df_producteurs._append({'film': title, 'producteur': producteur}, ignore_index=True)
+            for realisateur in info.get('Réalisateurs', '').split(', '):
+                df_realisateurs = df_realisateurs._append({'film': title, 'realisateur': realisateur}, ignore_index=True)
+            for scenariste in info.get('Scénaristes', '').split(', '):
+                df_scenaristes = df_scenaristes._append({'film': title, 'scenariste': scenariste}, ignore_index=True)
+            for pays in info.get('Pays d\'origine', '').split(', '):
+                df_pays = df_pays._append({'film': title, 'pays': pays}, ignore_index=True)
+            for genre in info.get('Genre', '').split(', '):
+                df_genres = df_genres._append({'film': title, 'genre': genre}, ignore_index=True)
+
+
+
+        return df_films, df_genres, df_producteurs, df_realisateurs, df_scenaristes, df_pays
+        
