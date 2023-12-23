65c3041060087d0b223b91bbf1128f627ef98fb9
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2023-12-23 16:58:38 +0100

    Correction Extraction & Transformation . TOD : fix details retrieving

--- etl/extract.py
+++ etl/extract.py
@@ -26,7 +26,7 @@ class BaseFilmExtractor:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        pagination = soup.find("nav", class_="Pagination__WrapperPagination-sc-1h0mvsr-0 dFTVxz")
+        pagination = soup.find("nav")
         
         if pagination:
             last = pagination.find_all("span")[-1].text
@@ -88,9 +88,7 @@ class BaseFilmExtractor:
         response = requests.get(detail_url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        div_titre = soup.find("div", class_="ProductDetails__WrapperTitle-sc-5nxbfw-5 hKtmtY")
-        if div_titre:
-            h1_titre = div_titre.find("h1", class_="Text__SCTitle-sc-1aoldkr-1 iTBZrv")
+        h1_titre = soup.find("h1", class_="sc-e6f263fc-1 bwGoop")
         if h1_titre:
             return h1_titre.text.strip()
 
@@ -101,7 +99,7 @@ class BaseFilmExtractor:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        div_note = soup.find("div", class_="Rating__GlobalRating-sc-1b09g9c-5 itJntU")
+        div_note = soup.find("div", class_="sc-8251ce8c-5 fdoSxm")
         if div_note:
             return float(div_note.text.strip())  
         return None
@@ -111,27 +109,23 @@ class BaseFilmExtractor:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        span_image = soup.find("span", class_="Poster__WrapperImage-sc-yale2-10 jPPcbD")
+        png_links = soup.find_all('link', attrs={'as': 'image', 'href': True})
+        png_urls = [link['href'] for link in png_links if link['href'].endswith('.png')]
 
-        data_srcname = None
-        if span_image and 'data-srcname' in span_image.attrs:
-            data_srcname = span_image.attrs['data-srcname']
-        return data_srcname
+        return png_urls[0] 
 
-    @staticmethod
-    def extract_reviews(url, is_negative=True, cpt = 4):
+    @classmethod
+    def extract_reviews(cls, url, is_negative=True, cpt = 100):
         if cpt == 0:
             raise Exception("Too many attempts")
         
         driver = webdriver.Chrome()
 
-        n = BaseFilmExtractor.calculate_total_pages(url)
-        print(n)
-
         review_url = f"{url}/critiques"
+
         driver.get(review_url)
 
-        time.sleep(5)
+        time.sleep(7)
         try:
             cookie_button = driver.find_element(By.ID, "didomi-notice-agree-button")
             cookie_button.click()
@@ -142,6 +136,13 @@ class BaseFilmExtractor:
         review_type = 'Négatives' if is_negative else 'Positives'
         link_reviews = driver.find_element(By.XPATH, f"//a[contains(text(), '{review_type}')]")
         link_reviews.click()
+        time.sleep(2)
+        try :
+            first_nav = driver.find_element(By.TAG_NAME, "nav")
+            last_span = first_nav.find_elements(By.TAG_NAME, "span")[-1]
+            n = int(last_span.text)
+        except:
+            n = 1
 
         all_links = []
         page_number = 1
@@ -149,23 +150,22 @@ class BaseFilmExtractor:
         while True:
             time.sleep(5)
 
-            elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'Text__SCText-sc-1aoldkr-0') and contains(@class, 'Link__PrimaryLink-sc-1v081j9-0') and contains(@class, 'gATBvI') and contains(@class, 'hJEAZk') and contains(@class, 'link')]")
+            elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'sc-e6f263fc-0') and contains(@class, 'sc-a0949da7-0') and contains(@class, 'iZcnfH') and contains(@class, 'cYbaKn') and contains(@class, 'link')]")
             links = [element.get_attribute('href') for element in elements]
             all_links.extend(links)
 
             try:
                 page_number += 1
-                next_page_button = driver.find_element(By.XPATH, f"//nav[contains(@class, 'Pagination__WrapperPagination-sc-1h0mvsr-0') and contains(@class, 'dFTVxz')]//span[normalize-space()='{page_number}']")
-                next_page_button.click()
+                page_span = first_nav.find_element(By.XPATH, f".//span[normalize-space(text())='{page_number}']")
+                page_span.click()
             except Exception as e:
-                if (page_number - 1) != n:
+                if (page_number - 1) == n:
+                    driver.quit()
+                    break
+                else:
                     c = cpt - 1
                     driver.quit()
                     BaseFilmExtractor.extract_reviews(url, is_negative, cpt=c)
-                else:
-                    print(f"Done with page {page_number - 1} for {url}")
-                    driver.quit()
-                    break
 
 
         return all_links
@@ -176,16 +176,16 @@ class BaseFilmExtractor:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        review_content = soup.find("div", class_="Content__Container-sc-kh4s73-0 eCjAM")
+        review_content = soup.find("div", class_="sc-cfcc05b8-0 fywlHd")
         content = review_content.find("p").text if review_content else "Content not found"
 
-        review_title = soup.find("h1", class_="Text__SCUserText-sc-1aoldkr-2 ReviewTitle__SCTitleStyle-sc-15v21rr-0 kRkjza cjQHgA Review__StyledReviewTitle-sc-197w87t-7 elSTZr")
+        review_title = soup.find("h1", class_="sc-e6f263fc-2 sc-b23687b5-0 ewlOkU gmTbwg sc-6b0a5f14-7 jPNYGd")
         title = review_title.text if review_title else "Title not found"
 
-        ctas = soup.find_all("div", class_="CTARoundTotal__Container-sc-abr50j-0 cbKjVp")
+        ctas = soup.find_all("div", class_="sc-ede15b33-0 bBMXjh")
         likes, comments = None, None
         if ctas:
-            spans = [cta.find("span", class_="Text__SCTitle-sc-1aoldkr-1 CTARoundTotal__Label-sc-abr50j-1 glKUks kqxwbN") for cta in ctas]
+            spans = [cta.find("span", class_="sc-e6f263fc-1 sc-ede15b33-1 erFyGe iblteA") for cta in ctas]
             if len(spans) >= 2:
                 likes = spans[0].text if spans[0] else 0
                 comments = spans[1].text if spans[1] else 0
@@ -226,13 +226,57 @@ class CurrentMovieExtractor(BaseFilmExtractor):
     def __init__(self, url=URL):
         super().__init__(url)
         self.url = url
+    
+    def extract_all_film_links(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        links = soup.find_all('a', class_='sc-e976169f-2 juEctl')
+
+        base_url = "https://www.senscritique.com/"
+        self.urls_films = [base_url + link.get('href') for link in links if link.get('href')]
+    
+    @classmethod
+    def extract_reviews(cls, url, is_negative=True, cpt = 100):
+        if cpt == 0:
+            raise Exception("Too many attempts")
+        
+        driver = webdriver.Chrome()
+
+        review_url = f"{url}/critiques"
+
+        driver.get(review_url)
+
+        time.sleep(7)
+        try:
+            cookie_button = driver.find_element(By.ID, "didomi-notice-agree-button")
+            cookie_button.click()
+            time.sleep(5)
+        except Exception as e:
+            raise Exception("Cookie button not found or already clicked")
+
+        review_type = 'Négatives' if is_negative else 'Positives'
+        link_reviews = driver.find_element(By.XPATH, f"//a[contains(text(), '{review_type}')]")
+        link_reviews.click()
+        time.sleep(2)
+        
+        all_links = []
+
+        time.sleep(5)
+
+        elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'sc-e6f263fc-0') and contains(@class, 'sc-a0949da7-0') and contains(@class, 'iZcnfH') and contains(@class, 'cYbaKn') and contains(@class, 'link')]")
+        links = [element.get_attribute('href') for element in elements]
+        all_links.extend(links)
+
+        return all_links
+
 
 
 class FilmTransformer:
 
     def __init__(self, extractor : BaseFilmExtractor):
         self.extractor = extractor
-        self.df_films, self.df_genres, self.df_producteurs, self.df_realisateurs, self.df_scenaristes, self.df_pays = self.__transform()
+        self.df_films, self.df_genres, self.df_producteurs, self.df_realisateurs, self.df_scenaristes, self.df_pays, self.df_reviews = self.__transform()
     
     def __transform(self):
         informations = self.extractor.informations
@@ -243,6 +287,7 @@ class FilmTransformer:
         df_realisateurs = pd.DataFrame(columns=['film', 'realisateur'])
         df_scenaristes = pd.DataFrame(columns=['film', 'scenariste'])
         df_pays = pd.DataFrame(columns=['film', 'pays'])
+        df_reviews = pd.DataFrame(columns=['film', 'is_negative', 'title', 'likes', 'comments', 'content', 'url'])
 
         for title, info in informations.items():
             for key in list(info.keys()):
@@ -283,5 +328,6 @@ class FilmTransformer:
 
 
 
-        return df_films, df_genres, df_producteurs, df_realisateurs, df_scenaristes, df_pays
+        return df_films, df_genres, df_producteurs, df_realisateurs, df_scenaristes, df_pays, df_reviews
+        
         
